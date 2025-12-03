e260497e17ab9ca4fd4a4f5b593f2ae90c836dd6
AizenLM-PC <diamanteludwing145@gmail.com>
2025-12-02 18:23:41 -0600

    Modificando url backend2

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,2 @@
-export const URL_SERVER = "https://electronic-carla-ludwingha-992b09de.koyeb.app/"
+export const URL_SERVER = "https://electronic-carla-ludwingha-992b09de.koyeb.app"
 export const URL_FRONTEND = "https://redsocialtec.netlify.app/login"
